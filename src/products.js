dd30cbaa21cdaa18f80be4202222145fe75fbc5c
Luca <ziotughi24@gmai.com>
2024-09-13 09:47:13 +0200

    q

--- src/products.js
+++ src/products.js
@@ -1,9 +1,9 @@
 // import rivista1 from './components/img/rivista1.png'
-import rivista2 from './components/img/rivista2.png'
-import rivista3 from './components/img/rivista3.png'
-import rivista4 from './components/img/rivista4.png'
-import rivista5 from './components/img/rivista5.png'
-import rivista6 from './components/img/rivista6.png'
+import rivista2 from './components/img/rivista2.2.png'
+import rivista3 from './components/img/rivista3.3.png'
+import rivista4 from './components/img/rivista4.4.png'
+import rivista5 from './components/img/rivista5.5.png'
+import rivista6 from './components/img/rivista1.png'
 
 
 export const PRODUCTS = [
@@ -17,39 +17,39 @@ export const PRODUCTS = [
 
     {
         id: 2,
-        productName: "All About Space - Pluton",
-        price: 0.2,
+        productName: "Science - Renewable Energy",
+        price: 0.002,
         productImage: rivista2
 
     },
 
     {
         id: 3,
-        productName: "All About Space - Otegezegenler",
-        price: 0.002,
+        productName: "Produice - Renewable Energy",
+        price: 0.005,
         productImage: rivista3
 
     },
 
     {
         id: 4,
-        productName: "All About Space - Kesifleri",
-        price: 0.003,
+        productName: "AI and Machine Learning",
+        price: 0.01,
         productImage: rivista4
 
     },
 
     {
         id: 5,
-        productName: "All About Space - Envren",
-        price: 0.001,
+        productName: "Best Science - The DNA",
+        price: 0.1,
         productImage: rivista5
 
     },
 
     {
         id: 6,
-        productName: "All About Space - Uzayda Yasam",
+        productName: "Scientice - The Space",
         price: 0.5,
         productImage: rivista6
 
