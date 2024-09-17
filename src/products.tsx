5d4df22d399b2a123df50e7853733760813bf033
Luca <ziotughi24@gmai.com>
2024-09-17 11:08:16 +0200

    tweaks

--- src/products.tsx
+++ src/products.tsx
@@ -1,8 +1,13 @@
-import rivista2 from './components/img/rivista2.2.png'
-import rivista3 from './components/img/rivista3.3.png'
-import rivista4 from './components/img/rivista4.4.png'
-import rivista5 from './components/img/rivista5.5.png'
-import rivista6 from './components/img/rivista1.png'
+import rivista1 from './components/img/rivista1.png'
+import rivista2 from './components/img/rivista2.png'
+import rivista3 from './components/img/rivista3.png'
+import rivista4 from './components/img/rivista4.png'
+import rivista5 from './components/img/rivista5.png'
+import rivista6 from './components/img/rivista6.png'
+import rivista7 from './components/img/rivista7.png'
+import rivista8 from './components/img/rivista8.png'
+import rivista9 from './components/img/rivista9.png'
+
 
 interface Product {
     id: number;
@@ -13,43 +18,77 @@ interface Product {
 
 export const PRODUCTS: Product[] = [
 
+
+
+    {
+        id: 1,
+        productName: "Spientice - Space Technology",
+        price: 0.5,
+        productImage: rivista1
+
+    },
+
     {
         id: 2,
-        productName: "Science - Renewable Energy",
-        price: 0.002,
+        productName: "Renewable Energy (edition 2)",
+        price: 0.005,
         productImage: rivista2
 
     },
 
     {
         id: 3,
-        productName: "Produice - Renewable Energy",
-        price: 0.005,
+        productName: "AI and Machine Learning",
+        price: 0.01,
         productImage: rivista3
 
     },
 
     {
         id: 4,
-        productName: "AI and Machine Learning",
-        price: 0.01,
+        productName: "Best Science - The DNA",
+        price: 0.1,
         productImage: rivista4
 
     },
 
     {
         id: 5,
-        productName: "Best Science - The DNA",
-        price: 0.1,
+        productName: "Renewable Energy (Ediotion 1)",
+        price: 0.002,
         productImage: rivista5
 
     },
 
     {
         id: 6,
-        productName: "Scientice - The Space",
-        price: 0.5,
+        productName: "Scientice - Neural Science",
+        price: 0.08,
         productImage: rivista6
+
+    },
+
+    {
+        id: 7,
+        productName: "Qeiuntfic - Quantum Computing",
+        price: 1.2,
+        productImage: rivista7
+
+    },
+
+    {
+        id: 8,
+        productName: "Scientice - Deep Sea",
+        price: 0.07,
+        productImage: rivista8
+
+    },
+
+    {
+        id: 9,
+        productName: "Scientice - Arropace Engineering",
+        price: 0.009,
+        productImage: rivista9
 
     },
     
